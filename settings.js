eee4753f9ff01cf32de00537d63d8833e4e6e9f7
michbeck <michbeck@MacBookPro-M1.local>
2026-01-18 20:21:03 +0100

    Alles speichern

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 const gameSettings = {
     gameTitle: "🚀 Kennst du IT 115??", // Name des Spiels
-    tileSize: "110px",           // Größe der Kacheln (z.B. 130px)
+    tileSize: "5px",           // Größe der Kacheln (z.B. 130px)
     gridColumns: "4"      // Grid-Muster, z.B. "auto-fit" oder eine feste Zahl wie 4
 };
