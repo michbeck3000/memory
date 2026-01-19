b6bf1ffe82d66b9237793d6b359c6d2e417b9702
michbeck <michbeck@MacBookPro-M1.local>
2026-01-19 10:50:24 +0100

    Revert tile size to 150px

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 const gameSettings = {
     gameTitle: "Kennst du IT 115??", // Name des Spiels
-    tileSize: "200px",           // Größe der Kacheln (z.B. 200px für iPad)
+    tileSize: "150px",           // Größe der Kacheln (z.B. 200px für iPad)
     gridColumns: "auto-fit"      // Grid-Muster, z.B. "auto-fit" oder eine feste Zahl wie 4
 };
